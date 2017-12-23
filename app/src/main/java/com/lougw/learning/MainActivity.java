5da752e06428e09be8a02fea07761f0cfb8e2e1a
gaoweilou <gaoweilou@pptv.com>
2017-12-23 23:24:17 +0800

    imageloader

--- app/src/main/java/com/lougw/learning/MainActivity.java
+++ app/src/main/java/com/lougw/learning/MainActivity.java
@@ -1,13 +1,34 @@
 package com.lougw.learning;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.lougw.learning.net.NetActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.net) Button btnNet;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.net})
+    public void Click(View v) {
+        if (v.getId() == R.id.net) {
+            Intent intent = new Intent(this, NetActivity.class);
+            startActivity(intent);
+        }
+
     }
 }
---
+++ app/src/main/java/com/lougw/learning/utils/ImageLoader.java
@@ -0,0 +1,57 @@
+package com.lougw.learning.utils;
+
+import android.content.Context;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader;
+import com.bumptech.glide.load.DecodeFormat;
+import com.bumptech.glide.load.model.GlideUrl;
+
+import java.io.InputStream;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+
+/**
+ * <pre>
+ *     author : lougw
+ *     e-mail : gaoweilou@pptv.com
+ *     time   : 2017/12/23
+ *     desc   :
+ * </pre>
+ */
+
+public class ImageLoader {
+
+    private ImageLoader() {
+    }
+
+    private static class Instance {
+        private static ImageLoader instance = new ImageLoader();
+    }
+
+    public static ImageLoader getInstance() {
+        return Instance.instance;
+    }
+
+    public void init(Context context) {
+        OkHttpClient builder = new OkHttpClient.Builder()
+                .connectTimeout(10, TimeUnit.SECONDS)
+                .readTimeout(10, TimeUnit.SECONDS)
+                .writeTimeout(10, TimeUnit.SECONDS)
+                .build();
+        Glide.get(context).register(GlideUrl.class, InputStream.class, new OkHttpUrlLoader.Factory(builder));
+    }
+
+    public void display(String uri, ImageView view) {
+        this.display(uri, view, 0, false);
+    }
+
+    public void display(String uri, ImageView view, final int defaultImage, boolean skip) {
+        Glide.with(UIUtils.getContext()).load(uri).asBitmap().dontAnimate().format(
+                DecodeFormat.PREFER_RGB_565).placeholder(defaultImage).error(defaultImage).skipMemoryCache(skip).into(view);
+    }
+}
+
+
