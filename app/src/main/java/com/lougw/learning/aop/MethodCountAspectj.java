4f7c41a2571982eefc97bc9b762f1f1c66322303
lougaowei <lougaowei@cmcm.com>
2018-11-07 16:05:35 +0800

    media

--- app/src/main/java/com/lougw/learning/aop/MethodCountAspectj.java
+++ app/src/main/java/com/lougw/learning/aop/MethodCountAspectj.java
@@ -2,9 +2,11 @@ package com.lougw.learning.aop;
 
 import android.util.Log;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 
@@ -22,25 +24,51 @@ public class MethodCountAspectj {
 
     }
 
-    @Around("invokeMethod() && !invokeWatch()")
+    //       @Around("invokeMethod() && !invokeWatch()")
     public void aroundMethodExecution(final ProceedingJoinPoint joinPoint) {
         try {
             MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
             String className = methodSignature.getDeclaringType().getSimpleName();
             String methodName = methodSignature.getName();
+            String stackTrace = getStackTrace();
             final StopWatch stopWatch = new StopWatch();
             stopWatch.start();
             joinPoint.proceed();
             stopWatch.stop();
-            Log.d(TAG, buildLogMessage(className, methodName, stopWatch.getTotalTimeMillis()));
+            Log.d(TAG, buildLogMessage(stackTrace, className, methodName, stopWatch.getTotalTimeMillis()));
         } catch (Throwable throwable) {
             throwable.printStackTrace();
         }
 
     }
 
-    public static String buildLogMessage(String className, String methodName, long methodDuration) {
+    @Pointcut("execution(* com.lougw.learning.modle.VideoDataInfo.get*(..))")
+    public void invokeClassMethod() {
+
+    }
+
+
+    @Before("invokeClassMethod()")
+    public void beforeMethodExecution(final JoinPoint joinPoint) {
+        try {
+            MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+            String className = methodSignature.getDeclaringType().getSimpleName();
+            String methodName = methodSignature.getName();
+            String stackTrace = getStackTrace();
+            final StopWatch stopWatch = new StopWatch();
+            Log.d(TAG, buildLogMessage(stackTrace, className, methodName, stopWatch.getTotalTimeMillis()));
+            getStackTrace();
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+
+    }
+
+
+    public static String buildLogMessage(String stackTrace, String className, String methodName, long methodDuration) {
         StringBuilder message = new StringBuilder();
+        message.append(" --> ");
+        message.append(stackTrace);
         message.append(" --> ");
         message.append(className);
         message.append(" --> ");
@@ -52,4 +80,28 @@ public class MethodCountAspectj {
         message.append("  ]");
         return message.toString();
     }
+
+    public static String getStackTrace() {
+        StringBuilder sb = new StringBuilder();
+        StackTraceElement[] stackTraceElements = (new Throwable()).getStackTrace();
+        for (int i = 0; i < stackTraceElements.length; i++) {
+            if (i == 3) {
+                StackTraceElement stackTraceElement = stackTraceElements[i];
+                if (stackTraceElement != null) {
+                    sb.append(stackTraceElement.getClassName());
+                    sb.append(".");
+                    sb.append(stackTraceElement.getMethodName());
+                    sb.append("( )");
+                    sb.append("-->( ");
+                    sb.append(stackTraceElement.getLineNumber());
+                    sb.append(" )");
+                }
+                break;
+            }
+            continue;
+        }
+        return sb.toString();
+    }
+
+
 }
