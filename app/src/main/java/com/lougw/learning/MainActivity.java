a72d07a7057bb5e760f7fe970e09c074d9a60766
lougaowei <lougaowei@cmcm.com>
2018-11-26 20:22:44 +0800

    pay

--- app/src/main/java/com/lougw/learning/MainActivity.java
+++ app/src/main/java/com/lougw/learning/MainActivity.java
@@ -11,6 +11,11 @@ import com.lougw.learning.media.MediaActivity;
 import com.lougw.learning.media.OpenGLActivity;
 import com.lougw.learning.net.NetActivity;
 import com.lougw.learning.utils.CheckLogin;
+import com.samsung.android.sdk.iap.lib.helper.HelperDefine;
+import com.samsung.android.sdk.iap.lib.helper.IapHelper;
+import com.samsung.android.sdk.iap.lib.listener.OnPaymentListener;
+import com.samsung.android.sdk.iap.lib.vo.ErrorVo;
+import com.samsung.android.sdk.iap.lib.vo.PurchaseVo;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -19,7 +24,13 @@ import butterknife.OnClick;
 public class MainActivity extends AppCompatActivity {
     @BindView(R.id.net)
     Button btnNet;
-
+    /**
+     * 支付
+     */
+    private IapHelper mIapHelper = null;
+    private static HelperDefine.OperationMode IAP_MODE = HelperDefine.OperationMode.OPERATION_MODE_PRODUCTION;
+    public static final String ITEM_ID_SUBSCRIPTION = "ARS";
+    public static final String PASS_THROUGH_PARAM = "000003742525";
 
     @CheckLogin
     @Override
@@ -31,7 +42,8 @@ public class MainActivity extends AppCompatActivity {
         getName();
         isHigh();
     }
-    @OnClick({R.id.net, R.id.shimmer, R.id.full_screen, R.id.web_server, R.id.rxjava, R.id.andfix, R.id.network, R.id.guid, R.id.download, R.id.opengl, R.id.media})
+
+    @OnClick({R.id.net, R.id.shimmer, R.id.full_screen, R.id.web_server, R.id.rxjava, R.id.andfix, R.id.network, R.id.guid, R.id.download, R.id.opengl, R.id.media, R.id.samsung})
     public void Click(View v) {
         if (v.getId() == R.id.net) {
             Intent intent = new Intent(this, NetActivity.class);
@@ -51,25 +63,37 @@ public class MainActivity extends AppCompatActivity {
         } else if (v.getId() == R.id.andfix) {
             Intent intent = new Intent(this, AndFixActivity.class);
             startActivity(intent);
-        }else if (v.getId() == R.id.network) {
+        } else if (v.getId() == R.id.network) {
             Intent intent = new Intent(this, NetActivity.class);
             startActivity(intent);
-        }else if (v.getId() == R.id.guid) {
+        } else if (v.getId() == R.id.guid) {
             Intent intent = new Intent(this, GUIDActivity.class);
             startActivity(intent);
-        }else if (v.getId() == R.id.download) {
+        } else if (v.getId() == R.id.download) {
             Intent intent = new Intent(this, DownloadActivity.class);
             startActivity(intent);
-        }else if (v.getId() == R.id.opengl) {
+        } else if (v.getId() == R.id.opengl) {
             Intent intent = new Intent(this, OpenGLActivity.class);
             startActivity(intent);
-        }else if (v.getId() == R.id.media) {
+        } else if (v.getId() == R.id.media) {
             Intent intent = new Intent(this, MediaActivity.class);
             startActivity(intent);
+        } else if (v.getId() == R.id.samsung) {
+            mIapHelper = IapHelper.getInstance(this.getApplicationContext());
+            mIapHelper.setOperationMode(IAP_MODE);
+            mIapHelper.startPayment(ITEM_ID_SUBSCRIPTION,
+                    PASS_THROUGH_PARAM,
+                    false,
+                    new OnPaymentListener() {
+                        @Override
+                        public void onPayment(ErrorVo _errorVO, PurchaseVo _purchaseVO) {
+                            Log.d("LgwTag", "" + _errorVO.getErrorString());
+                        }
+                    });
+
         }
 
     }
-
 
     @Override
     protected void onStart() {
