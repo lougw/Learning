d571df02f042b79f876bbcdc9db2a0019c05e928
lougaowei <lougaowei@chehejia.com>
2019-02-12 19:13:24 +0800

    bright

--- app/src/main/java/com/lougw/learning/ScreenBrightnessActivity.java
+++ app/src/main/java/com/lougw/learning/ScreenBrightnessActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -44,6 +45,8 @@ public class ScreenBrightnessActivity extends AppCompatActivity {
     }
 
     private void setWindowBrightness(float brightness) {
+        Log.d("LgwTag","brightness : "+brightness);
+
         Window window = getWindow();
         WindowManager.LayoutParams lp = window.getAttributes();
         lp.screenBrightness = brightness;
